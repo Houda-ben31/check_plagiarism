f562423c841a28b64ed494ff92c329dee650e8b3
Nina Developer <hodaorania@gmail.com>
2025-11-08 19:28:08 +0100

    Initial commit - FastAPI plagiarism checker

--- palgarism.js
+++ palgarism.js
@@ -13,7 +13,7 @@ document.getElementById("checkButton").addEventListener("click", async function(
 
     try {
         // إرسال النص إلى Python API
-        const response = await fetch("http://127.0.0.1:8000/api/check", {
+        const response = await fetch("https://check-plagiarism.onrender.com/api/check", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ content: text })
